3aa7fcca6857c5f63305ee1f96a232cb9015f1ec
Boris Cherny <boris@performancejs.com>
2015-01-26 16:36:17 -0800

    worker can now use

--- src/twerk-worker.js
+++ src/twerk-worker.js
@@ -1,6 +1,7 @@
 // mock stuff for angular
 var document = {
   addEventListener: function(){},
+  cookie: '',
   createElement: function (){
     return {
       addEventListener: function(){},
@@ -18,31 +19,40 @@ var window = {
     href: '',
     url: ''
   },
-  state: {},
   history: {
     state: {}
   },
-  url: {}
+  clearTimeout: this.clearTimeout,
+  setTimeout: this.setTimeout,
+  XMLHttpRequest: this.XMLHttpRequest
 }
 
-this.state = {}
-
+// load angular
 importScripts('../bower_components/angular/angular.js')
 
 // not this is a sandboxed module, and has no knowledge of the main twerk module
 window.angular
 .module('twerk', [])
-.run(function ($http, $log) {
+.run(['$http', '$log', function ($http, $log) {
 
   console.info('initialized twerkworker', this.performance.now())
 
   onmessage = function onMessage (e) {
 
-    console.log('message', e)
+    console.info('twerkworker got message', e)
+
+    var method = e.data[0].toLowerCase()
+
+    if (!$http[method]) {
+      throw new Error('HTTP method not supported')
+      return
+    }
+
+    $http[e.data[0]]
 
   }
 
-}.bind(this))
+}.bind(this)])
 
 // since the worker can't interact with the DOM,
 // we need to manually bootstrap our app
